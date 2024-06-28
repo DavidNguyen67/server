8e4ba0edc508279085d1cf1370d76aa9b99f1ee7
David Nguyễn <davidnguyen67dev@gmail.com>
2024-06-28 22:07:21 +0700

    ↩ revert(Trước khi thêm keycloack):
    Sau khi đã thêm các entities

--- src/main/java/com/david/server/controllers/mysql/UsersController.java
+++ src/main/java/com/david/server/controllers/mysql/UsersController.java
@@ -2,23 +2,38 @@
  * @Author         : David Nguyễn <davidnguyen67dev@gmail.com>
  * @CreatedDate    : 2024-06-22 19:30:00
  * @LastEditors    : David Nguyễn <davidnguyen67dev@gmail.com>
- * @LastEditDate   : 2024-06-23 18:03:53
+ * @LastEditDate   : 2024-06-22 19:30:07
  * @CopyRight      : Con chù chù 🥴🥴
 **/
 
 package com.david.server.controllers.mysql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.david.server.database.services.mysql.UsersService;
 
 /**
  * REST controller for managing User entities.
  * This controller provides endpoints to interact with user data.
  */
-@RestController()
+@RestController
 @RequestMapping("users")
 public class UsersController {
+
+  /**
+   * The service used to manage user data.
+   */
+  @Autowired
+  private UsersService usersService;
+
+  /**
+   * Endpoint to get a list of users.
+   *
+   * @return A JSON array of user names.
+   */
   @GetMapping
   public String list() {
     return "[\"Joe\", \"Peter\"]";
